22af0afc2c7603a8c0dc8ef28e184adc72080c3f
Yusuke Kobayashi <yusukeko@usc.edu>
2025-05-03 05:13:54 +0000

    hash-functions fixed

--- hash.h
+++ hash.h
@@ -20,7 +20,33 @@ struct MyStringHash {
     HASH_INDEX_T operator()(const std::string& k) const
     {
         // Add your code here
+			unsigned long long w[5] = {0, 0, 0, 0, 0};
+			int len = k.length();
 
+			for(int i=0; i<len; i++){
+
+				int wIndex = 4-(i/6);
+
+				int posInChunk = i%6;
+
+				unsigned long long charValue = letterDigitToNumber(k[len -1 -i]);
+
+				unsigned long long factor = 1;
+
+				for(int j=0; j<posInChunk; j++){
+					factor *= 36;
+				}
+
+				w[wIndex] += charValue*factor;
+			}
+
+			unsigned long long result =0;
+
+			for(int i=0; i<5; i++){
+				result += rValues[i]*w[i];
+			}
+
+			return result;
 
     }
 
@@ -28,7 +54,17 @@ struct MyStringHash {
     HASH_INDEX_T letterDigitToNumber(char letter) const
     {
         // Add code here or delete this helper function if you do not want it
+			if(letter >= 'A' && letter<= 'Z'){
+				letter = letter-'A'+'a';
+			}
 
+			if(letter >= 'a' && letter<='z'){
+				return letter-'a';
+			}else if(letter >= '0'&& letter<='9'){
+				return letter - '0' + 26;
+			}
+
+			return 0;
     }
 
     // Code to generate the random R values
